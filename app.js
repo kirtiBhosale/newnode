e3f59970ef44e69c273c9f46c2e22e33b0749cb4
Kirti Bhosale - Network <bhosale_kirti@network.elanco.com>
2020-07-16 08:47:16 +0200

    my commit

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ require('dotenv').config();
 express.static('public')
 
 app.get('/', function(req, res){
-  res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
+ // res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
+ res.send("hello world");
 });
-app.post('/', function(req, res) {
-    var item = req.body.userSearchInput;
-    console.log(item);
-});
+//app.post('/', function(req, res) {
+  //  var item = req.body.userSearchInput;
+    //console.log(item);
+//});
 
 http.listen(process.env.port, function(){
   console.log('listening on *:3000');
