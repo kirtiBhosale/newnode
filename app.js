a523b952c52f17785a498afaa1243427a4731b9b
Kirti Bhosale - Network <bhosale_kirti@network.elanco.com>
2020-07-23 02:20:28 -0400

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 express.static('public')
 
 app.get('/', function(req, res){
- // res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
+ res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
  res.send("hello world in my first");
  res.send("my first program");
 });
